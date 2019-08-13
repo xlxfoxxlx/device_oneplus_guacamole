AOSiP-Devices/device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Use utils from base
Change-Id: I9909efbf3187e0650b757305915e873a844c379c
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
@@ -24,31 +24,31 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.aosip.FileFileUtils;
 
 public class DCIModeSwitch implements OnPreferenceChangeListener {
 
     private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_p3_mode";
 
     public static String getFile() {
-        if (Utils.fileWritable(FILE)) {
+        if (FileUtils.fileWritable(FILE)) {
             return FILE;
         }
         return null;
     }
 
     public static boolean isSupported() {
-        return Utils.fileWritable(getFile());
+        return FileUtils.fileWritable(getFile());
     }
 
     public static boolean isCurrentlyEnabled(Context context) {
-        return Utils.getFileValueAsBoolean(getFile(), false);
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         Boolean enabled = (Boolean) newValue;
-        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
         return true;
     }
 }

xlxfoxxlx/device_oneplus_guacamole

DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
@@ -24,31 +24,31 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.aosip.FileUtils;
 
 public class DCIModeSwitch implements OnPreferenceChangeListener {
 
     private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_p3_mode";
 
     public static String getFile() {
-        if (Utils.fileWritable(FILE)) {
+        if (FileUtils.fileWritable(FILE)) {
             return FILE;
         }
         return null;
     }
 
     public static boolean isSupported() {
-        return Utils.fileWritable(getFile());
+        return FileUtils.fileWritable(getFile());
     }
 
     public static boolean isCurrentlyEnabled(Context context) {
-        return Utils.getFileValueAsBoolean(getFile(), false);
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         Boolean enabled = (Boolean) newValue;
-        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
         return true;
     }
 }