AOSiP-Devices/device_oneplus_sm8150-common

sm8150-common: Introduce DeviceSettings
combine DisplayMode and KeyHandler to one category containing all device specific options

Change-Id: I3f1ae8adb9e86db1a505acb43c837bbeea6f984b
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

DeviceSettings/src/com/aosip/device/DeviceSettings/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aosip.device.DeviceSettings;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.main,
+                    DeviceSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_additional_buttons),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.aosip.device.DeviceSettings";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

xlxfoxxlx/device_oneplus_guacamole

DeviceSettings/src/com/aosip/device/DeviceSettings/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aosip.device.DeviceSettings;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.main,
+                    DeviceSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_device),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.aosip.device.DeviceSettings";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}